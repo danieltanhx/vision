c9f4448737a51d7660c4482ac06c1c469e5b15ac
danieltanhx <60249161+danieltanhx@users.noreply.github.com>
2023-10-30 11:03:47 +0800

    Update train.py

--- references/classification/train.py
+++ references/classification/train.py
@@ -21,9 +21,8 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, arg
     metric_logger = utils.MetricLogger(delimiter="  ")
     metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value}"))
     metric_logger.add_meter("img/s", utils.SmoothedValue(window_size=10, fmt="{value}"))
-    args.print_freq=20
     header = f"Epoch: [{epoch}]"
-    for i, (image, target) in enumerate(metric_logger.log_every(data_loader, args.print_freq, header)):
+    for i, (image, target) in enumerate(metric_logger.log_every(data_loader, 20, header)):
         start_time = time.time()
         image, target = image.to(device), target.to(device)
         with torch.cuda.amp.autocast(enabled=scaler is not None):
